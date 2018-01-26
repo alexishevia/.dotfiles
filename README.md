eb72aa4302fa7e9e1ffc8eae0aedc7877e54a265
Alexis Hevia <hevia.alexis@gmail.com>
2018-01-25 23:15:10 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# sysconfig
+scripts to configure my dev environment
+
+This scripts should allow me to reconstruct my entire development environment on a blank Ubuntu machine.
+
+## Usage
+```
+git clone https://github.com/alexishevia/.sysconfig ~/.sysconfig
+~/.sysconfig/run.sh
+```
---
+++ run.sh
@@ -0,0 +1,21 @@
+# apt update
+sudo apt update
+
+# install apt packages I use
+sudo apt install --yes curl build-essential git git-core gitk ack-grep ncurses-term xclip dconf-tools unzip libjpeg62 libwebkitgtk-1.0-0 unrar compizconfig-settings-manager s3cmd compiz-plugins sni-qt:i386 gcolor2 screenruler vim-gtk android-tools-adb android-tools-fastboot tmux rbenv tidy exuberant-ctags synapse httpie jq
+
+# remove apt packages I don't want
+sudo apt remove --yes avahi-daemon vim vim-gnome terminator
+
+# apt cleanup
+sudo apt autoclean --yes
+sudo apt autoremove --yes
+
+# install nvm
+if ! [ -d ~/.nvm ]; then
+  git clone https://github.com/creationix/nvm.git ~/.nvm
+fi
+pushd ~/.nvm
+git pull
+. ~/.nvm/nvm.sh
+popd
