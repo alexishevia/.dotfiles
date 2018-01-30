52f5f0fae9ad490e7cbd88429ea35b1a87993dd9
Alexis Hevia <hevia.alexis@gmail.com>
2018-01-29 20:56:42 -0500

    add f.lux

--- apt.sh
+++ apt.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# f.lux
+sudo add-apt-repository --yes ppa:nathan-renniewaldock/flux
+
 # apt update
 sudo apt update
 
 # install apt packages I use
-sudo apt install --yes curl build-essential git git-core gitk ack-grep ncurses-term xclip dconf-tools unzip libjpeg62 libwebkitgtk-1.0-0 unrar compizconfig-settings-manager s3cmd compiz-plugins sni-qt:i386 gcolor2 screenruler vim-gtk android-tools-adb android-tools-fastboot tmux rbenv tidy exuberant-ctags synapse httpie jq
+sudo apt install --yes curl build-essential git git-core gitk ack-grep ncurses-term xclip dconf-tools unzip libjpeg62 libwebkitgtk-1.0-0 unrar compizconfig-settings-manager s3cmd compiz-plugins sni-qt:i386 gcolor2 screenruler vim-gtk android-tools-adb android-tools-fastboot tmux rbenv tidy exuberant-ctags synapse httpie jq fluxgui
 
 # remove apt packages I don't want
 sudo apt remove --yes avahi-daemon vim vim-gnome terminator
