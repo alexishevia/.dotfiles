a7f4013dd08d32986176c63695b5e85bfd908166
Alexis Hevia <hevia.alexis@gmail.com>
2018-02-25 17:08:00 -0500

    use snap to install packages

--- apt.sh
+++ apt.sh
@@ -12,21 +12,31 @@ sudo add-apt-repository --yes ppa:unit193/encryption
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 
-# skype
-echo 'deb [arch=amd64] https://repo.skype.com/deb stable main' | sudo tee /etc/apt/sources.list.d/skype-stable.list
-
 # charles proxy
 wget -q -O - https://www.charlesproxy.com/packages/apt/PublicKey | sudo apt-key add -
 echo 'deb https://www.charlesproxy.com/packages/apt/ charles-proxy main' | sudo tee /etc/apt/sources.list.d/charles.list
+
+# dropbox
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+echo 'deb http://linux.dropbox.com/ubuntu/ xenial main' | sudo tee /etc/apt/sources.list.d/dropbox.list
+
+# openshot
+sudo add-apt-repository ppa:openshot.developers/ppa
 
 # apt update
 sudo apt update
 
 # install apt packages I use
-sudo apt install --yes curl build-essential git git-core gitk ack-grep ncurses-term xclip dconf-tools unzip libjpeg62 libwebkitgtk-1.0-0 unrar compizconfig-settings-manager s3cmd compiz-plugins sni-qt:i386 gcolor2 screenruler vim-gtk android-tools-adb android-tools-fastboot tmux rbenv tidy exuberant-ctags synapse httpie jq fluxgui google-chrome-stable charles-proxy skypeforlinux silversearcher-ag veracrypt
+sudo apt install --yes curl build-essential git git-core gitk ack-grep ncurses-term xclip dconf-tools unzip libjpeg62 libwebkitgtk-1.0-0 unrar compizconfig-settings-manager s3cmd compiz-plugins sni-qt:i386 gcolor2 screenruler vim-gtk android-tools-adb android-tools-fastboot rbenv tidy exuberant-ctags synapse fluxgui google-chrome-stable charles-proxy silversearcher-ag veracrypt dropbox openshot
 
 # remove apt packages I don't want
 sudo apt remove --yes avahi-daemon vim vim-gnome terminator
+
+# install snap packages I use
+sudo snap install slack --classic
+sudo snap install tmux --classic
+sudo snap install skype --classic
+sudo snap install spotify http jq
 
 # apt cleanup
 sudo apt autoclean --yes
