30b0b739dc9c10ba297f69c2a766bd0ec510b1b2
Alexis Hevia <hevia.alexis@gmail.com>
2018-05-17 10:25:59 -0500

    do not install dropbox by default

--- apt.sh
+++ apt.sh
@@ -38,7 +38,7 @@ echo 'deb https://www.charlesproxy.com/packages/apt/ charles-proxy main' | sudo 
 
 # dropbox
 # sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
-echo 'deb http://linux.dropbox.com/ubuntu/ xenial main' | sudo tee /etc/apt/sources.list.d/dropbox.list
+# echo 'deb http://linux.dropbox.com/ubuntu/ xenial main' | sudo tee /etc/apt/sources.list.d/dropbox.list
 
 # apt update
 sudo apt update
