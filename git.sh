9d70b18a95dbbd9f67d2adb4dce1f006ab28f820
Alexis Hevia <hevia.alexis@gmail.com>
2018-05-17 10:26:30 -0500

    update git repos

--- git.sh
+++ git.sh
@@ -3,11 +3,13 @@
 set -e # exit if any command fails
 
 # clone repos I use
-repos="$HOME/.sysconfig?git@github.com:alexishevia/.sysconfig.git
+repos="$HOME/.dotfiles?git@github.com:alexishevia/.dotfiles.git
 $HOME/knowledge?git@bitbucket.org:alexishevia/knowledge.git
-$HOME/Projects/Personales/faru?git@github.com:alexishevia/faru.git
-$HOME/Projects/Personales/personal_finance?git@bitbucket.org:alexishevia/personal_finance.git
-$HOME/Projects/FOX/dcgapi-services?git@github.com:foxbroadcasting/dcgapi-services.git"
+$HOME/jupyter_notebooks?git@github.com:alexishevia/jupyter_notebooks.git
+$HOME/Projects/FOX/dcgapi-services?git@github.com:foxbroadcasting/dcgapi-services.git
+$HOME/Projects/FOX/fng-cms?git@github.com:foxbroadcasting/fng-cms.git
+$HOME/Projects/FOX/splunk_dashboards?git@github.com:foxbroadcasting/splunk_dashboards.git
+$HOME/Projects/Personales/personal_finance?git@bitbucket.org:alexishevia/personal_finance.git"
 for repo in $repos; do
   IFS='?' read -a arr <<< $repo
   dir="${arr[0]}"
