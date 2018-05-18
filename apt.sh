92d95bf31fc5c418a946811af98b7680ab0d46e6
Alexis Hevia <hevia.alexis@gmail.com>
2018-05-17 21:16:03 -0500

    add gnome-screensaver

--- apt.sh
+++ apt.sh
@@ -51,7 +51,7 @@ sudo apt install --yes curl build-essential git git-core gitk ack-grep        \
   veracrypt openshot golang-1.10-go gnupg2 libzmq5 libzmq3-dev                \
   apt-transport-https sublime-text heroku mono-complete openshot dkms         \
   libxcb-xtest0 ttf-ancient-fonts python-pip gnome-tweak-tool albert          \
-  docker.io docker-compose
+  docker.io docker-compose gnome-screensaver
 
 # remove apt packages I don't want
 sudo apt remove --yes avahi-daemon vim vim-gnome terminator
