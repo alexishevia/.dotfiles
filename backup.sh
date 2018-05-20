4658078d8acadc749f6e04e849629bb85654a042
Alexis Hevia <hevia.alexis@gmail.com>
2018-05-20 10:20:14 -0500

    move git repos to the constants.sh file

--- backup.sh
+++ backup.sh
@@ -2,15 +2,10 @@
 
 set -e # exit if any command fails
 
+source ./constants.sh
+
 # make sure repos have no pending changes
-repos="$HOME/.dotfiles?git@github.com:alexishevia/.dotfiles.git
-$HOME/knowledge?git@bitbucket.org:alexishevia/knowledge.git
-$HOME/jupyter_notebooks?git@github.com:alexishevia/jupyter_notebooks.git
-$HOME/Projects/FOX/dcgapi-services?git@github.com:foxbroadcasting/dcgapi-services.git
-$HOME/Projects/FOX/fng-cms?git@github.com:foxbroadcasting/fng-cms.git
-$HOME/Projects/FOX/splunk_dashboards?git@github.com:foxbroadcasting/splunk_dashboards.git
-$HOME/Projects/Personales/personal_finance?git@bitbucket.org:alexishevia/personal_finance.git"
-for repo in $repos; do
+for repo in $REPOS; do
   IFS='?' read -a arr <<< $repo
   dir="${arr[0]}"
 
---
+++ constants.sh
@@ -0,0 +1,8 @@
+REPOS="$HOME/.dotfiles?git@github.com:alexishevia/.dotfiles.git
+$HOME/knowledge?git@bitbucket.org:alexishevia/knowledge.git
+$HOME/jupyter_notebooks?git@github.com:alexishevia/jupyter_notebooks.git
+$HOME/Projects/FOX/dcgapi-services?git@github.com:foxbroadcasting/dcgapi-services.git
+$HOME/Projects/FOX/fng-cms?git@github.com:foxbroadcasting/fng-cms.git
+$HOME/Projects/FOX/splunk_dashboards?git@github.com:foxbroadcasting/splunk_dashboards.git
+$HOME/Projects/Personales/personal_finance?git@bitbucket.org:alexishevia/personal_finance.git
+$HOME/go/src/github.com/alexishevia/spotify-party?git@github.com:alexishevia/spotify-party.git"
