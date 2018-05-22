9564aa4ed9aa914ee1a62cf8af00c7cc88ce277e
Alexis Hevia <hevia.alexis@gmail.com>
2018-05-22 11:35:24 -0500

    add 'network-manager-openvpn-gnome'

--- apt.sh
+++ apt.sh
@@ -48,7 +48,7 @@ sudo apt install --yes curl build-essential git git-core gitk ack-grep        \
   apt-transport-https sublime-text heroku mono-complete openshot dkms         \
   libxcb-xtest0 ttf-ancient-fonts python-pip gnome-tweak-tool albert          \
   docker.io docker-compose gnome-screensaver openvpn vlc postgresql-10        \
-  libgconf-2-4
+  libgconf-2-4 network-manager-openvpn-gnome
 
 # remove apt packages I don't want
 sudo apt remove --yes avahi-daemon vim vim-gnome terminator
