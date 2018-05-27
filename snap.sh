bd58841bc20bbce0a1c641357914d4c644062f40
Alexis Hevia <hevia.alexis@gmail.com>
2018-05-27 14:14:01 -0500

    add obs-studio

--- snap.sh
+++ snap.sh
@@ -12,7 +12,7 @@ sudo snap install sublime-text --classic
 sudo snap install go --classic
 sudo snap install heroku --classic
 sudo snap install spotify http jq robomongo gimp inkscape libreoffice chromium \
-  vlc postgresql10
+  vlc postgresql10 obs-studio
 
 # refresh already installed snaps
 sudo snap refresh
