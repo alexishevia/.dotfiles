d36578e95e4224fd5b8aa100698e7b09267a36ff
Alexis Hevia <hevia.alexis@gmail.com>
2018-05-27 20:13:55 -0500

    install virtualbox

--- apt.sh
+++ apt.sh
@@ -21,6 +21,12 @@ wget -nv -O /tmp/Release.key https://build.opensuse.org/projects/home:manuelschn
 sudo apt-key add - < /tmp/Release.key
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 
+# virtualbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+codename=$(lsb_release --codename | cut -f2)
+echo "deb https://download.virtualbox.org/virtualbox/debian ${codename} contrib" | sudo tee /etc/apt/sources.list.d/dropbox.list
+
 # apt update
 sudo apt update
 
@@ -31,7 +37,7 @@ sudo apt install --yes curl build-essential git git-core gitk ack-grep        \
   synapse charles-proxy silversearcher-ag sni-qt:i386 veracrypt               \
   gnupg2 libzmq5 libzmq3-dev screenruler apt-transport-https mono-complete    \
   dkms libgconf-2-4 libxcb-xtest0 ttf-ancient-fonts python-pip python3-pip    \
-  docker.io docker-compose gnome-screensaver albert                           \
+  docker.io docker-compose gnome-screensaver albert virtualbox-5.2            \
   gnome-tweak-tool openvpn network-manager-openvpn-gnome
 
 # remove apt packages I don't want
