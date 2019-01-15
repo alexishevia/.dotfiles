fe08535de111752a81e36eef5557c35af932ec28
Alexis Hevia <hevia.alexis@gmail.com>
2019-01-15 11:15:14 -0500

    Use eslint-config-prettier to prevent eslint from showing style-related errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
-  extends: ['airbnb'],
+  extends: ['prettier', 'plugin:require-path-exists/recommended'],
+  plugins: ['require-path-exists'],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
   rules: {
-    indent: ['error', 2, {
-      MemberExpression: 0, // indent the multi-line property chains with 0 spaces
-    }],
-    'require-jsdoc': [0],
-    'valid-jsdoc': ['warn'],
+    'no-undef': ['error'],
+    'no-unused-vars': ['error'],
     'no-param-reassign': ['error', {
       props: true,
       ignorePropertyModificationsFor: ['memo', 'acc'], // allow reducer accumulators to be modified
     }],
     'complexity': ['error', 10], // allow max cyclomatic complexity of 10
+    'require-jsdoc': [0],
+    'valid-jsdoc': ['warn'],
   },
 };
--- nvm.sh
+++ nvm.sh
@@ -17,10 +17,10 @@ versions='8.12.0 10.15.0'
 for version in $versions; do
   nvm install $version
   nvm use $version
-  npm install -g npm yarn lerna timezones-cli ppp-cli json eslint \
-  eslint-config-airbnb commitizen eslint-plugin-jsx-a11y eslint-plugin-react \
-  eslint-plugin-import typescript flow-bin @foxdcg/is-commit-deployed \
-  @foxdcg/reindex-mpx-item @mprokopowicz/sqs-monitor elasticdump mdo-cli
+  npm install -g npm yarn lerna timezones-cli ppp-cli json commitizen \
+  typescript flow-bin elasticdump mdo-cli @foxdcg/reindex-mpx-item \
+  @mprokopowicz/sqs-monitor \
+  eslint eslint-config-prettier eslint-plugin-require-path-exists
 done
 
 nvm alias default 8.12.0
