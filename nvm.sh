5dc797aee5d448f48c164bf98b42891b345cbe88
Alexis Hevia <hevia.alexis@gmail.com>
2019-04-01 16:38:04 -0500

    update knowledge directory

--- nvm.sh
+++ nvm.sh
@@ -17,10 +17,11 @@ versions='8.14.0 10.15.0'
 for version in $versions; do
   nvm install $version
   nvm use $version
-  npm install -g npm yarn lerna timezones-cli ppp-cli json commitizen \
-  typescript flow-bin elasticdump mdo-cli @foxdcg/reindex-mpx-item \
-  @mprokopowicz/sqs-monitor \
-  eslint eslint-config-prettier eslint-plugin-require-path-exists
+  npm install -g npm yarn lerna commitizen typescript flow-bin elasticdump \
+    eslint eslint-config-prettier eslint-plugin-require-path-exists \
+    timezones-cli ppp-cli json @mdo-org/mdo-cli \
+    @foxdcg/reindex-mpx-item @mprokopowicz/sqs-monitor \
+    docsify-cli
 done
 
 nvm alias default 8.14.0
