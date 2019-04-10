4f15b5ee13a7c6128f35d861ba21afecc79af765
Alexis Hevia <hevia.alexis@gmail.com>
2019-04-10 16:57:59 -0500

    modify fresh-chrome to use chromium

--- snap.sh
+++ snap.sh
@@ -11,7 +11,7 @@ sudo snap install kubectl --classic
 sudo snap install sublime-text --classic
 sudo snap install heroku --classic
 sudo snap install http jq robomongo gimp inkscape libreoffice \
-  vlc postgresql10 obs-studio simplescreenrecorder ngrok
+  vlc postgresql10 obs-studio simplescreenrecorder ngrok chromium
 
 # refresh already installed snaps
 sudo snap refresh
