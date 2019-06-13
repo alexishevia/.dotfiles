37a4718c4362dbb089aa901da55934c0df9fc9b7
Alexis Hevia <hevia.alexis@gmail.com>
2019-06-13 15:11:43 -0500

    update REPOS

--- constants.sh
+++ constants.sh
@@ -1,8 +1,6 @@
 REPOS="$HOME/.dotfiles?git@github.com:alexishevia/.dotfiles.git
-$HOME/knowledge?git@bitbucket.org:alexishevia/knowledge.git
-$HOME/jupyter_notebooks?git@github.com:alexishevia/jupyter_notebooks.git
-$HOME/Projects/FOX/dcgapi-services?git@github.com:foxbroadcasting/dcgapi-services.git
-$HOME/Projects/FOX/fng-cms?git@github.com:foxbroadcasting/fng-cms.git
-$HOME/Projects/FOX/splunk_dashboards?git@github.com:foxbroadcasting/splunk_dashboards.git
+$HOME/knowledge-base?git@github.com:alexishevia/knowledge-base.git
+$HOME/Projects/Personales/mdo?git@github.com:mdo-org/mdo.git
+$HOME/Projects/Personales/mdo-native?git@github.com:mdo-org/mdo-native.git
 $HOME/Projects/Personales/personal_finance?git@bitbucket.org:alexishevia/personal_finance.git
-$HOME/go/src/github.com/alexishevia/spotify-party?git@github.com:alexishevia/spotify-party.git"
+$HOME/Projects/Personales/zettelkasten?git@github.com:alexishevia/zettelkasten.git"
