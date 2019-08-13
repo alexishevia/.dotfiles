c6b72eb58920466990b6447b5173b1af57bf7e83
Alexis Hevia <hevia.alexis@gmail.com>
2019-08-13 08:04:43 -0500

    update prey version

--- apps/prey.sh
+++ apps/prey.sh
@@ -3,6 +3,6 @@
 set -e # exit if any command fails
 
 if [ ! -d /etc/prey ]; then
-  wget -O /tmp/prey.deb 'https://downloads.preyproject.com/prey-client-releases/node-client/1.7.5/prey_1.7.5_amd64.deb'
+  wget -O /tmp/prey.deb 'https://downloads.preyproject.com/prey-client-releases/node-client/1.9.1/prey_1.9.1_amd64.deb'
   sudo dpkg -i /tmp/prey.deb
 fi
