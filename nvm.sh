8e0ca2aeecd1fa4769a40a7e20316e257c83a2bb
Alexis Hevia <hevia.alexis@gmail.com>
2019-09-18 17:56:46 -0500

    add node v10.15.2

--- nvm.sh
+++ nvm.sh
@@ -12,7 +12,7 @@ git pull
 popd
 
 
-versions='8.14.0 10.15.0'
+versions='10.15.0 10.15.2'
 
 for version in $versions; do
   nvm install $version
@@ -24,5 +24,5 @@ for version in $versions; do
     docsify-cli onchange expo-cli vsce
 done
 
-nvm alias default 10.15.0
-nvm use 10.15.0
+nvm alias default 10.15.2
+nvm use 10.15.2
