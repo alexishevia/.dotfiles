b580001c292356a5133112e31dbff58693a82920
Alexis Hevia <hevia.alexis@gmail.com>
2019-12-07 23:00:54 -0500

    add foxbroadcasting repos to constants

--- constants.sh
+++ constants.sh
@@ -1,6 +1,27 @@
+mkdir -p $HOME/go/src/github.com/foxbroadcasting/
+mkdir -p $HOME/Projects/Personales/
+
 REPOS="$HOME/.dotfiles?git@github.com:alexishevia/.dotfiles.git
 $HOME/knowledge-base?git@github.com:alexishevia/knowledge-base.git
 $HOME/Projects/Personales/mdo?git@github.com:mdo-org/mdo.git
 $HOME/Projects/Personales/mdo-native?git@github.com:mdo-org/mdo-native.git
 $HOME/Projects/Personales/personal_finance?git@bitbucket.org:alexishevia/personal_finance.git
-$HOME/Projects/Personales/zettelkasten?git@github.com:alexishevia/zettelkasten.git"
+$HOME/Projects/Personales/zettelkasten?git@github.com:alexishevia/zettelkasten.git
+$HOME/go/src/github.com/foxbroadcasting/cpe-liveplayer?git@github.com:foxbroadcasting/cpe-liveplayer.git
+$HOME/go/src/github.com/foxbroadcasting/cpe-liveplayer?git@github.com:foxbroadcasting/cpe-liveplayer
+$HOME/go/src/github.com/foxbroadcasting/cpe-lp-listings?git@github.com:foxbroadcasting/cpe-lp-listings
+$HOME/go/src/github.com/foxbroadcasting/cpe-lp-videos?git@github.com:foxbroadcasting/cpe-lp-videos
+$HOME/go/src/github.com/foxbroadcasting/cpe-lp-listings-shows?git@github.com:foxbroadcasting/cpe-lp-listings-shows
+$HOME/go/src/github.com/foxbroadcasting/cpe-lp-url?git@github.com:foxbroadcasting/cpe-lp-url
+$HOME/go/src/github.com/foxbroadcasting/cpe-mediasources?git@github.com:foxbroadcasting/cpe-mediasources
+$HOME/go/src/github.com/foxbroadcasting/cpe-asset-info?git@github.com:foxbroadcasting/cpe-asset-info
+$HOME/go/src/github.com/foxbroadcasting/cpe-lpsu?git@github.com:foxbroadcasting/cpe-lpsu
+$HOME/go/src/github.com/foxbroadcasting/cpe-psu?git@github.com:foxbroadcasting/cpe-psu
+$HOME/go/src/github.com/foxbroadcasting/cpe-station-locator?git@github.com:foxbroadcasting/cpe-station-locator
+$HOME/go/src/github.com/foxbroadcasting/cpe-geo-service?git@github.com:foxbroadcasting/cpe-geo-service
+$HOME/go/src/github.com/foxbroadcasting/cpe-dma?git@github.com:foxbroadcasting/cpe-dma
+$HOME/go/src/github.com/foxbroadcasting/cpe-listings?git@github.com:foxbroadcasting/cpe-listings
+$HOME/go/src/github.com/foxbroadcasting/cpe-assetvideo?git@github.com:foxbroadcasting/cpe-assetvideo
+$HOME/go/src/github.com/foxbroadcasting/cpe-assetimage?git@github.com:foxbroadcasting/cpe-assetimage
+$HOME/go/src/github.com/foxbroadcasting/cpe-contentsku?git@github.com:foxbroadcasting/cpe-contentsku
+$HOME/go/src/github.com/foxbroadcasting/cpe-canIplay?git@github.com:foxbroadcasting/cpe-canIplay"
