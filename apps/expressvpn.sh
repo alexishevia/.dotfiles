a6d02194b1f4a665d92abdef114c376c2ffaf645
Alexis Hevia <hevia.alexis@gmail.com>
2020-04-16 14:01:19 -0500

    update expressvpn

--- apps/expressvpn.sh
+++ apps/expressvpn.sh
@@ -3,7 +3,7 @@
 set -e # exit if any command fails
 
 if ! [ -x "$(command -v expressvpn)" ]; then
-  wget -O /tmp/expressvpn.deb 'https://download.expressvpn.xyz/clients/linux/expressvpn_2.4.4.19-1_amd64.deb'
+  wget -O /tmp/expressvpn.deb 'https://download.expressvpn.xyz/clients/linux/expressvpn_2.4.5.2-1_amd64.deb'
   sudo dpkg -i /tmp/expressvpn.deb
   expressvpn activate
 fi
