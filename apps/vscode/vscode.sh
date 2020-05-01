f99408503826d3493225c68f43d7cef1b039ef76
Alexis Hevia <hevia.alexis@gmail.com>
2020-05-01 15:03:24 -0500

    add vscode extensions

--- apps/vscode/vscode.sh
+++ apps/vscode/vscode.sh
@@ -2,17 +2,21 @@
 
 set -e # exit if any command fails
 
-files='settings.json keybindings.json'
 path="$HOME/.config/Code/User"
-
 if ! [ -e $path ]; then
    mkdir -p $path
 fi
 
+files='settings.json keybindings.json'
 for file in $files; do
   if [ -e $path/$file ]
   then
     rm $path/$file
   fi
   ln -s "$PWD/apps/vscode/$file" $path/$file
+done
+
+extensions='vscodevim.vim ms-vscode.go eamodio.gitlens alanwalk.markdown-toc alexishevia.mdo-extension-code'
+for extension in $extensions; do
+  code --install-extension $extension
 done
