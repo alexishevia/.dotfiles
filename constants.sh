7df88c92040c63311d2647ef2086749125f0d8b5
Alexis Hevia <hevia.alexis@gmail.com>
2020-05-13 21:47:06 -0500

    Add 'cpe-epglistings' repo

--- constants.sh
+++ constants.sh
@@ -34,4 +34,5 @@ $HOME/go/src/github.com/foxbroadcasting/cpe-contentsku?git@github.com:foxbroadca
 $HOME/go/src/github.com/foxbroadcasting/cpe-foxentitlement?git@github.com:foxbroadcasting/cpe-foxentitlement.git
 $HOME/go/src/github.com/foxbroadcasting/cpe-canIplay?git@github.com:foxbroadcasting/cpe-canIplay
 $HOME/go/src/github.com/foxbroadcasting/cpe-cli?git@github.com:foxbroadcasting/cpe-cli.git
+$HOME/go/src/github.com/foxbroadcasting/cpe-epglistings?git@github.com:foxbroadcasting/cpe-epglistings.git
 $HOME/go/src/github.com/foxbroadcasting/cpe-sportsplayer?git@github.com:foxbroadcasting/cpe-sportsplayer.git"
