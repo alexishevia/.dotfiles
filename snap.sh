97a7daa2337a6f1bafe3183d9acb74e6d9269c37
Alexis Hevia <hevia.alexis@gmail.com>
2020-06-22 20:11:34 -0500

    move each snap package to a separate line

--- snap.sh
+++ snap.sh
@@ -3,14 +3,34 @@
 set -e # exit if any command fails
 
 # install snap packages I use
-PACKAGES="slack skype asciinema kubectl sublime-text heroku android-studio"
+PACKAGES=""
+PACKAGES+=" slack"
+PACKAGES+=" skype"
+PACKAGES+=" asciinema"
+PACKAGES+=" kubectl"
+PACKAGES+=" sublime-text"
+PACKAGES+=" heroku"
+PACKAGES+=" android-studio"
 for pkg in $PACKAGES; do
   if ! [ -x "$(command -v $pkg)" ]; then
     sudo snap install $pkg --classic
   fi
 done
 
-PACKAGES="http jq robomongo gimp inkscape libreoffice vlc obs-studio simplescreenrecorder ngrok chromium spotify"
+PACKAGES=""
+PACKAGES+=" http"
+PACKAGES+=" jq"
+PACKAGES+=" robomongo"
+PACKAGES+=" gimp"
+PACKAGES+=" inkscape"
+PACKAGES+=" libreoffice"
+PACKAGES+=" vlc"
+PACKAGES+=" obs-studio"
+PACKAGES+=" simplescreenrecorder"
+PACKAGES+=" ngrok"
+PACKAGES+=" chromium"
+PACKAGES+=" spotify"
+PACKAGES+=" orion-desktop" # Powerful BitTorrent client and torrent streamer for Linux Desktop
 for pkg in $PACKAGES; do
   if ! [ -x "$(command -v $pkg)" ]; then
     sudo snap install $pkg
