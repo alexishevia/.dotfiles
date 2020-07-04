9ff0160915af417d94cc593ed4e58dbb24831423
Alexis Hevia <hevia.alexis@gmail.com>
2020-07-04 18:32:13 -0500

    add commitizen

--- nvm.sh
+++ nvm.sh
@@ -17,9 +17,13 @@ versions='12.16.1 v11.9.0'
 for version in $versions; do
   nvm install $version
   nvm use $version
-  npm install -g npm yarn timezones-cli json @mdo-org/mdo-cli \
-    eslint eslint-config-prettier eslint-plugin-require-path-exists \
-    docsify-cli onchange expo-cli vsce
+  npm install -g npm yarn
+  npm install -g eslint eslint-config-prettier eslint-plugin-require-path-exists
+  npm install -g timezones-cli # Command line tool to convert a date or timestamp into different timezones.
+  npm install -g @mdo-org/mdo-cli # free, open source to-do app built to live inside your editor
+  npm install -g docsify-cli # A magical documentation generator
+  npm install -g vsce # Visual Studio Code Extension Manager
+  npm install -g commitizen # Simple commit conventions for internet citizens.
 done
 
 nvm alias default 12.16.1
