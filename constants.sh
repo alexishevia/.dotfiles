81829f9e9b2ab33dd14f61c9f09ad411db9448b7
Alexis Hevia <hevia.alexis@gmail.com>
2020-09-17 12:30:14 -0500

    Add the 'cpe-video-service' repository

--- constants.sh
+++ constants.sh
@@ -40,4 +40,5 @@ $HOME/go/src/github.com/foxbroadcasting/cpe-epglistings?git@github.com:foxbroadc
 $HOME/go/src/github.com/foxbroadcasting/cpe-sportsplayer?git@github.com:foxbroadcasting/cpe-sportsplayer.git
 $HOME/go/src/github.com/foxbroadcasting/tracking-data-helper?git@github.com:foxbroadcasting/tracking-data-helper.git
 $HOME/go/src/github.com/foxbroadcasting/go-helpers?git@github.com:foxbroadcasting/go-helpers.git
-$HOME/go/src/github.com/foxbroadcasting/dcg-gomodels?git@github.com:foxbroadcasting/dcg-gomodels.git"
+$HOME/go/src/github.com/foxbroadcasting/dcg-gomodels?git@github.com:foxbroadcasting/dcg-gomodels.git
+$HOME/go/src/github.com/foxbroadcasting/cpe-video-service?git@github.com:foxbroadcasting/cpe-video-service.git"
