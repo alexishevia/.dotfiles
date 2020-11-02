4f2fcbb4fb6eb56dc90000f3a3714b93c7bbcf32
Alexis Hevia <hevia.alexis@gmail.com>
2020-11-02 16:50:11 -0500

    add cpe-sportsplayer config

--- work/work.sh
+++ work/work.sh
@@ -2,7 +2,7 @@
 
 set -e # exit if any command fails
 
-mockfiles='cpe-asset-info cpe-epglistings cpe-psu cpe-lp-videos cpe-lp-url cpe-liveplayer cpe-lp-listings cpe-geo-service'
+mockfiles='cpe-asset-info cpe-epglistings cpe-psu cpe-lp-videos cpe-lp-url cpe-liveplayer cpe-lp-listings cpe-geo-service cpe-sportsplayer'
 
 for file in $mockfiles; do
   path="$GOPATH/src/github.com/foxbroadcasting/$file/config/mock.json"
