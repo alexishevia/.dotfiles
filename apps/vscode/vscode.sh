e87a10ed41d6d0a8666b5024dd6cbaf2f797a491
Alexis Hevia <hevia.alexis@gmail.com>
2020-11-14 12:32:53 -0500

    update vscode.sh to use powershell

--- apps/vscode/vscode.sh
+++ apps/vscode/vscode.sh
@@ -2,23 +2,17 @@
 
 set -e # exit if any command fails
 
-path="$HOME/.config/Code/User"
+# path="$HOME/.config/Code/User"
+path='/mnt/c/Users/hevia/AppData/Roaming/Code/User'
 if ! [ -e $path ]; then
    mkdir -p $path
 fi
 
-files='settings.json keybindings.json'
-for file in $files; do
-  if [ -e $path/$file ]
-  then
-    rm $path/$file
-  fi
-  ln -s "$PWD/apps/vscode/$file" $path/$file
-done
+powershell.exe -command 'New-Item -ItemType SymbolicLink -Path C:\Users\hevia\AppData\Roaming\Code\User -Name settings.json -Value \\wsl$\Ubuntu-20.04\home\alexishevia\.dotfiles\apps\vscode\settings.json'
 
-# wsl cannot install vscode extensions
-# see: https://github.com/microsoft/vscode-remote-release/issues/2589
-# extensions='vscodevim.vim golang.go eamodio.gitlens alanwalk.markdown-toc alexishevia.mdo-extension-code esbenp.prettier-vscode mikestead.dotenv'
-# for extension in $extensions; do
-#   code --install-extension $extension
-# done
+powershell.exe -command 'New-Item -ItemType SymbolicLink -Path C:\Users\hevia\AppData\Roaming\Code\User -Name keybindings.json -Value \\wsl$\Ubuntu-20.04\home\alexishevia\.dotfiles\apps\vscode\keybindings.json'
+
+extensions='vscodevim.vim golang.go eamodio.gitlens alanwalk.markdown-toc alexishevia.mdo-extension-code esbenp.prettier-vscode mikestead.dotenv'
+for extension in $extensions; do
+  powershell.exe -command "code --install-extension $extension"
+done
